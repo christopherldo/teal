f4a6b4ee8774d5f587d0ed0b19b91d33caa5e7b2
Christopher Leonardo de Oliveira <christopherldo@AirdeChstopher2.lan>
2023-10-05 22:07:19 -0400

    :tada: Project setup
    Finishes packages configuration and adds missing files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,29 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [
+      {
+        lightTheme: {
+          primary: "#f4aa3a",
+          secondary: "#f4f4a1",
+          accent: "#1be885",
+          neutral: "#272136",
+          "base-100": "#ffffff",
+          info: "#778ad4",
+          success: "#23b893",
+          warning: "#f79926",
+          error: "#ea535a",
+          body: {
+            "background-color": "#e3e6e6",
+          },
+        },
+      },
+    ],
+  },
 }
+
 export default config
